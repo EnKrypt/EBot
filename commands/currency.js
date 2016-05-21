d2eb17eb63eed71929dfc8794b0914cade75618a
Arvind Kumar <arvind@enkrypt.in>
2016-05-21 11:54:57 +0530

    Fixed formatting in !currency.

--- commands/currency.js
+++ commands/currency.js
@@ -3,12 +3,12 @@
 var request=require('then-request');
 
 module.exports = function(context){
-	if (!context.args.length<3){
+	if (context.args.length>2){
 		request('GET', 'https://api.fixer.io/latest?base='+context.args[1]+'&symbols='+context.args[2]).done(function(response){
 			if (response.getBody('utf8')){
-				var rate=parseInt(JSON.parse(response.getBody('utf8')).rates[context.args[2]]);
+				var rate=parseFloat(JSON.parse(response.getBody('utf8')).rates[context.args[2]]);
 				try{
-					return parseInt(context.args[0])*rate;
+					context.client.say(context.to, context.args[0]+" "+context.args[1]+" is "+(parseFloat(context.args[0])*rate)+" "+context.args[2]);
 				}
 				catch(e){
 					return "Argument <amount> must be a number";
