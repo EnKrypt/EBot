c383c465917f8fccc24ba92b62b06a7e8004096e
Arvind Kumar <arvind@enkrypt.in>
2016-05-21 16:07:04 +0530

    Better error handling in !currency command.

--- commands/currency.js
+++ commands/currency.js
@@ -4,18 +4,28 @@ var request=require('then-request');
 
 module.exports = function(context){
 	if (context.args.length>2){
-		request('GET', 'https://api.fixer.io/latest?base='+context.args[1]+'&symbols='+context.args[2]).done(function(response){
-			if (response.getBody('utf8')){
-				var rate=parseFloat(JSON.parse(response.getBody('utf8')).rates[context.args[2]]);
+		request('GET', 'https://api.fixer.io/latest?base='+encodeURIComponent(context.args[1])+'&symbols='+encodeURIComponent(context.args[2])).done(function(response){
+			if (response.body.toString('utf8')){
+				var rate=0.0;
+				try{
+					rate=parseFloat(JSON.parse(response.body.toString('utf8')).rates[context.args[2]]);
+					if (isNaN(rate)){
+						throw new Error('There was no rate');
+					}
+				}
+				catch(e){
+					context.client.say(context.to, "Invalid currency code");
+					return;
+				}
 				try{
 					context.client.say(context.to, context.args[0]+" "+context.args[1]+" is "+(parseFloat(context.args[0])*rate)+" "+context.args[2]);
 				}
 				catch(e){
-					return "Argument <amount> must be a number";
+					context.client.say(context.to, "Argument <amount> must be a number");
 				}
 			}
 			else{
-				return "Could not connect to API";
+				context.client.say(context.to, "Could not connect to API");
 			}
 		});
 	}
